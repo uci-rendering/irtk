8cc83c8e09296308ce54eedbf4b67457c1113316
GuangyanCai <g5cai@ucsd.edu>
2022-06-03 01:26:43 -0700

    Created a simple scene representation and a simple test.

---
+++ ivt/scene.py
@@ -0,0 +1,94 @@
+import torch
+
+class Scene:
+
+    def __init__(self, ftype=torch.float32, itype=torch.long, device='cuda'):
+        self.integrator = None 
+        self.render_options = None 
+        self.film = None 
+
+        self.params = {}
+        self.params['sensors'] = []
+        self.params['meshes'] = []
+        self.params['bsdfs'] = []
+        self.params['emitters'] = []
+
+        self.ftype = ftype
+        self.itype = itype
+        self.device = device
+
+    def to_ftensor(self, array):
+        return torch.tensor(array, dtype=self.ftype, device=self.device) if array is not None else None
+
+    def to_itensor(self, array):
+        return torch.tensor(array, dtype=self.itype, device=self.device) if array is not None else None
+
+    def add_integrator(self, type):
+        self.integrator = {
+            'type': type
+        }
+
+    def add_render_options(self, options):
+        self.render_options = options
+
+    def add_hdr_film(self, resolution, rfilter='tent', crop=(0, 0, 1, 1)):
+        self.film = {
+            'type': 'hdr',
+            'resolution': resolution,
+            'rfilter': 'tent',
+            'crop': crop
+        }
+
+    def add_perspective_camera(self, fov, origin, target, up):
+        sensor = {
+            'type': type,
+            'fov': self.to_ftensor(fov),
+            'origin': self.to_ftensor(origin),
+            'target': self.to_ftensor(target),
+            'up': self.to_ftensor(up)
+        }
+        self.params['sensors'].append(sensor)
+
+    def add_mesh(self, vertex_positions, vertex_indices, bsdf_id, vertex_normals=None, uv_positions=None, uv_indices=None, to_world=torch.eye(4)):
+        mesh = {
+            'vertex_positions': self.to_ftensor(vertex_positions),
+            'vertex_indices': self.to_itensor(vertex_indices),
+            'vertex_normals': self.to_ftensor(vertex_normals),
+            'uv_positions': self.to_ftensor(uv_positions),
+            'uv_indices': self.to_ftensor(uv_indices),
+            'to_world': self.to_ftensor(to_world),
+            'bsdf_id': bsdf_id,
+        }
+        self.params['meshes'].append(mesh)
+
+    def add_diffuse(self, reflectance):
+        bsdf = {
+            'type': 'diffuse',
+            'reflectance': self.to_ftensor(reflectance)
+        }
+        self.params['bsdfs'].append(bsdf)
+    
+    def add_area_light(self, mesh_id, radiance):
+        emitter = {
+            'type': 'area',
+            'mesh_id': mesh_id,
+            'radiance': self.to_ftensor(radiance)
+        }
+        self.params['emitters'].append(emitter)
+
+    def requires_grad(self, param_type, id, prop, req=True):
+        data = self.params[param_type][id][prop]
+        assert torch.is_tensor(data)
+        data.requires_grad = req
+
+    def __repr__(self):
+        s = f'integrator: {self.integrator}\n'
+        s += f'render_options: {self.render_options}\n'
+        s += f'flim: {self.film}\n'
+        s += f'params: {self.params}'
+
+        return s
+
+    def validate(self):
+        pass
+    
---
+++ tests/scene_test.py
@@ -0,0 +1,35 @@
+from ivt.scene import Scene
+from pathlib import Path
+import igl
+import torch
+
+tests = []
+def add_test(func):
+    def wrapper():
+        print(f'Test ({func.__name__}) starts.\n')
+        func()
+        print(f'\nTest ({func.__name__}) ends.')
+    tests.append(wrapper)
+
+@add_test
+def test0():
+    bunny_path = Path('data', 'meshes', 'bunny.obj')
+    v, tc, n, f, ftc, fn = igl.read_obj(str(bunny_path))
+
+    scene = Scene(device='cpu')
+    scene.add_integrator('path')
+    scene.add_render_options({
+        'spp': 1
+    })
+    scene.add_hdr_film(resolution=(512, 512))
+    scene.add_perspective_camera(fov=45, origin=(1, 0, 0), target=(0, 0, 0), up=(0, 0, 1))
+    scene.add_mesh(v, f, 0)
+    scene.add_diffuse(torch.tensor((1, 0, 0)).reshape(1, 1, 3))
+    scene.add_area_light(mesh_id=0, radiance=(10, 10, 10))
+    scene.requires_grad('meshes', 0, 'vertex_positions')
+
+    print(scene)
+
+if __name__ == '__main__':
+    for test in tests:
+        test()
