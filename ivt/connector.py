10ad04db716d1f4ff18a846159809788e257db86
Guangyan Cai <gcai3@uci.edu>
2022-06-24 21:50:16 +0000

    Created a plugin system for connectors.

--- ivt/connector.py
+++ ivt/connector.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import stevedore.extension
 
 class Connector(ABC):
     
@@ -9,3 +10,17 @@ class Connector(ABC):
     @abstractmethod
     def renderD(self, scene, image_grads, sensor_ids=[0]):
         pass
+
+class ConnectorManager:
+
+    def __init__(self):
+        self.em = stevedore.extension.ExtensionManager('ivt_connectors')
+
+    def is_available(self, connector_name):
+        return connector_name in self.em
+
+    def get_availability_list(self):
+        return [ext.name for ext in self.em]
+
+    def get_connector(self, connector_name):
+        return self.em[connector_name].plugin()
