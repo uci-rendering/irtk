8ba75c1acffa2be2b17445acc41730f8ff77f45d
Guangyan Cai <gcai3@uci.edu>
2022-06-27 17:40:20 +0000

    Updated tests.

---
+++ tests/common.py
@@ -0,0 +1,42 @@
+from ivt.scene import Scene
+from ivt.io import read_obj
+
+from pathlib import Path
+import numpy as np
+
+def simple_scene():
+    meshes_path = Path('tests', 'scenes', 'bunny', 'meshes')
+    
+    scene = Scene(backend='numpy')
+    
+    scene.add_integrator('direct')
+    
+    scene.add_render_options({
+        'seed': 42,
+        'num_samples': 32,
+        'max_bounces': 1,
+        'num_samples_primary_edge': 4,
+        'num_samples_secondary_edge': 4,
+        'quiet': False
+    })
+    
+    scene.add_hdr_film(resolution=(512, 512))
+    
+    scene.add_perspective_camera(fov=45, origin=(0, 0, 30), target=(0, 0, 0), up=(0, 1, 0))
+    
+    v, f = read_obj(meshes_path / 'bunny.obj')
+    scene.add_mesh(v, f, 0)
+    
+    v, f = read_obj(meshes_path / 'light_0.obj')
+    scene.add_mesh(v, f, 1)
+    
+    v, f = read_obj(meshes_path / 'light_1.obj')
+    scene.add_mesh(v, f, 1)
+    
+    scene.add_diffuse_bsdf(np.array((0.8, 0.8, 0.8)).reshape(1, 1, 3))
+    scene.add_diffuse_bsdf(np.array((0.8, 0.8, 0.8)).reshape(1, 1, 3))
+    
+    scene.add_area_light(mesh_id=1, radiance=(50, 100, 80))
+    scene.add_area_light(mesh_id=2, radiance=(100, 70, 50))
+    
+    return scene
--- tests/scene_test.py
+++ tests/scene_test.py
@@ -1,4 +1,5 @@
-from ivt.scene import Scene
+from common import *
+
 from pathlib import Path
 import igl
 import torch
@@ -14,20 +15,7 @@ def add_test(func):
 
 @add_test
 def different_backends():
-    
-    bunny_path = Path('data', 'meshes', 'bunny.obj')
-    v, tc, n, f, ftc, fn = igl.read_obj(str(bunny_path))
-
-    scene = Scene(device='cpu')
-    scene.add_integrator('path')
-    scene.add_render_options({
-        'spp': 1
-    })
-    scene.add_hdr_film(resolution=(512, 512))
-    scene.add_perspective_camera(fov=45, origin=(1, 0, 0), target=(0, 0, 0), up=(0, 0, 1))
-    scene.add_mesh(v, f, 0)
-    scene.add_diffuse_bsdf(torch.tensor((1, 0, 0)).reshape(1, 1, 3))
-    scene.add_area_light(mesh_id=0, radiance=(10, 10, 10))
+    scene = simple_scene()
     
     print('backend: torch + cpu')
     print(scene)
