b7cd30071cf5c4f35ec7a8d79453eea92bc989a6
Guangyan Cai <gcai3@uci.edu>
2022-06-28 19:53:59 +0000

    Updated the scene parser test.

--- tests/scene_parser_test.py
+++ tests/scene_parser_test.py
@@ -1,10 +1,14 @@
 import ivt.scene_parser
 from ivt.scene_parser import SceneParserManager
+from ivt.connector import ConnectorManager
+from ivt.io import write_png
 from common import *
 
 from pathlib import Path
 import torch
 import numpy as np
+import os
+import psdr_cpu
 
 tests = []
 def add_test(func):
@@ -20,12 +24,41 @@ def mitsuba():
     spm = SceneParserManager()
     assert spm.is_available(scene_parser_name)
     sp = spm.get_scene_parser(scene_parser_name)
+
+    cm = ConnectorManager()
+    assert cm.is_available('psdr_enzyme')
+    connector = cm.get_connector('psdr_enzyme')
     
     output_path = Path('tmp_output', 'scene_parser_test', 'mitsuba')
     output_path.mkdir(parents=True, exist_ok=True)
 
+    # Connector + ivt scene
+    print('Rendering the ivt scene representation with a psdr-enzyme connector...')
     scene = simple_scene()
+    image_a = connector.renderC(scene)[0]
+    write_png(output_path / 'image_a.png', image_a)
+
+    # Mitsuba scene + psdr_cpu
+    print('Rendering a scene file with psdr-enzyme directly...')
     sp.write(output_path / 'scene.xml', scene)
+    old_path = os.getcwd()
+    os.chdir(output_path)
+    psdr_scene = psdr_cpu.Scene('scene.xml')
+    os.chdir(old_path)
+    integrator = psdr_cpu.Direct()
+    render_options = psdr_cpu.RenderOptions(
+        scene.render_options['seed'],
+        scene.render_options['num_samples'],
+        scene.render_options['max_bounces'],
+        scene.render_options['num_samples_primary_edge'],
+        scene.render_options['num_samples_secondary_edge'],
+        scene.render_options['quiet'],
+    )
+    width, height = scene.film['resolution']
+    image_b = integrator.renderC(psdr_scene, render_options).reshape(width, height, 3)
+    write_png(output_path / 'image_b.png', image_b)
+
+    print(f'L1 loss: {np.abs(image_a - image_b).mean()}')
 
 if __name__ == '__main__':
     for test in tests:
