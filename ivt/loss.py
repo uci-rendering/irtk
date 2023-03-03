af28e0bcc02addbae76d21ae64994d30268bb15c
Guangyan Cai <gcai3@uci.edu>
2023-03-03 13:54:55 -0800

    Added total variation loss.

--- ivt/loss.py
+++ ivt/loss.py
@@ -45,3 +45,9 @@ def mesh_laplacian_smoothing(verts, faces):
     loss = L.mm(verts) * norm_w - verts
     loss = loss.norm(dim=1)
     return loss.mean()
+
+def total_variation_loss(texture, texture_mask):
+    m = (texture_mask[:-1, :-1] & texture_mask[1:, :-1] & texture_mask[:-1, 1:])
+    loss = (texture[:-1, :-1] - texture[1:, :-1])[m].pow(2).mean() +\
+           (texture[:-1, :-1] - texture[:-1, 1:])[m].pow(2).mean()
+    return loss
