14cf29d662e81e4ede9c129814a0cd8d828e9dfc
Guangyan Cai <gcai3@uci.edu>
2023-05-09 16:37:22 -0700

    Commented out some unused import.

--- ivt/__init__.py
+++ ivt/__init__.py
@@ -1,8 +1,8 @@
 from .connector import *
-try:
-    from .connectors.psdr_cuda_connector import PSDRCudaConnector
-except:
-    pass
+# try:
+#     from .connectors.psdr_cuda_connector import PSDRCudaConnector
+# except:
+#     pass
 
 try:
     from .connectors.psdr_enzyme_connector import PSDREnzymeConnector
@@ -14,5 +14,5 @@ try:
 except:
     pass
 
-from .scene_parser import *
-from .scene_parsers.mitsuba_parser import MitsubaParser
+# from .scene_parser import *
+# from .scene_parsers.mitsuba_parser import MitsubaParser
