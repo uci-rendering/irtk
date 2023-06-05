353d0ef8572a61cce397be5718a22b81601e96a0
Guangyan Cai <gcai3@uci.edu>
2023-06-05 03:05:27 -0700

    Replaced environment.yml and setup.py with pyproject.toml.

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name='ivt',
-      version='0.1',
-      description='ivt',
-      url='https://github.com/uci-rendering/inv-render-toolkit',
-      packages=find_packages(),
-     )
