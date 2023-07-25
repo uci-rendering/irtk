e7ec61fbecf647dd5995898ef689bc6c91951f0a
Guangyan Cai <guangyanc@adobe.com>
2023-07-25 04:52:16 +0000

    Fixed io.read_mesh: gpytoolbox.read_mesh requires a str as path.

--- ivt/io.py
+++ ivt/io.py
@@ -9,7 +9,7 @@ from pathlib import Path
 import gpytoolbox
 
 def read_mesh(mesh_path):
-    v, f, uv, fuv = gpytoolbox.read_mesh(mesh_path, return_UV=True)
+    v, f, uv, fuv = gpytoolbox.read_mesh(str(mesh_path), return_UV=True)
     if uv.size == 0:
         uv = np.zeros((0, 2))
         fuv = np.zeros((0, 3))
