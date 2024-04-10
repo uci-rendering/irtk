88b4764970016ea607a51843a01dacda39d1fc2a
Guangyan Cai <gcai3@uci.edu>
2024-04-09 22:15:38 -0700

    Add better fake texture coordinates if they are not present.

--- irtk/io.py
+++ irtk/io.py
@@ -13,8 +13,8 @@ imageio.plugins.freeimage.download()
 def read_mesh(mesh_path):
     v, f, uv, fuv = gpytoolbox.read_mesh(str(mesh_path), return_UV=True)
     if uv.size == 0:
-        uv = np.zeros((1, 2))
-        fuv = np.zeros((1, 3))
+        uv = np.zeros((v.shape[0], 2))
+        fuv = f.copy()
     return v, f, uv, fuv
 
 def write_mesh(mesh_path, v, f, uv=None, fuv=None):
